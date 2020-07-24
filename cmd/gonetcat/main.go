3dc2f0c81e5e6f2fb6a192b83bd67bdc78876206
sean <sean.large1@gmail.com>
2020-07-24 16:11:31 -0400

    inital commit

---
+++ cmd/gonetcat/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+"log"
+"net/http"
+"time"
+
+"github.com/gorilla/mux"
+)
+
+func main() {
+	mtx := mux.NewRouter()
+	mtx.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello World!"))
+	})
+
+	srv := &http.Server{
+		Handler:      mtx,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
